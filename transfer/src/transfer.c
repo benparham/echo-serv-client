876f1f081b419e0c0a2d68ab67a4aa6c82d91b7e
Ben Parham <benparham391@gmail.com>
2014-06-09 12:25:14 -0400

    Finished off tf_recv

--- transfer/src/transfer.c
+++ transfer/src/transfer.c
@@ -35,6 +35,15 @@ static int wait_for_signal(int socket_fd, TF_SIGNAL desired_sig) {
 	return 0;
 }
 
+static int send_signal(int socket_fd, TF_SIGNAL sig) {
+	tf_signal signal = {MAGIC_CHECK, sig};
+	if (send(socket_fd, &signal, sizeof(tf_signal), 0) == -1) {
+		return 1;
+	}
+
+	return 0;
+}
+
 static int min(int x, int y) {
 	int result = (x < y) ? x : y;
 	return result;
@@ -42,18 +51,18 @@ static int min(int x, int y) {
 
 int tf_send(int socket_fd, const void *buf, size_t size_bytes) {
 
-	printf("Preparing to send %d bytes\n", (int) size_bytes);
+	// printf("Preparing to send %d bytes\n", (int) size_bytes);
 
 	// Create and send header
 	tf_header header = {MAGIC_CHECK, size_bytes};
 	send(socket_fd, &header, sizeof(tf_header), 0);
-	printf("Sent header\n");
+	// printf("Sent header\n");
 
 	// Wait for confirmation signal
 	if (wait_for_signal(socket_fd, GOT_HEADER)) {
 		return 1;
 	}
-	printf("Received confirmation signal: GOT HEADER\n");
+	// printf("Received confirmation signal: GOT HEADER\n");
 
 	// Iteratively send all the data
 	int bytes_to_send = size_bytes;
@@ -68,26 +77,28 @@ int tf_send(int socket_fd, const void *buf, size_t size_bytes) {
 		bytes_to_send -= bytes_this_round;
 		next = next + bytes_this_round;
 	}
-	printf("Sent all %d bytes\n", (int) size_bytes);
+	// printf("Sent all %d bytes\n", (int) size_bytes);
 
 	// Wait for confirmation signal
 	if (wait_for_signal(socket_fd, GOT_DATA)) {
 		return 1;
 	}
-	printf("Received confirmation signal: GOT DATA\n");
+	// printf("Received confirmation signal: GOT DATA\n");
 
 	return 0;
 }
 
 int tf_recv(int socket_fd, void **buf, size_t *size_bytes, int *term) {
 
+	int bytes_received;
+
 	*term = 0;
 
-	printf("Waiting to receive bytes\n");
+	// printf("Waiting to receive bytes\n");
 
 	// Wait for header
 	tf_header header;
-	int bytes_received = recv(socket_fd, &header, sizeof(tf_header), 0);
+	bytes_received = recv(socket_fd, &header, sizeof(tf_header), 0);
 
 	if (bytes_received == 0) {
 		*term = 1;
@@ -98,7 +109,12 @@ int tf_recv(int socket_fd, void **buf, size_t *size_bytes, int *term) {
 		header.magic != MAGIC_CHECK) {
 		goto exit;
 	}
-	printf("Received header, bytes = %d\n", header.bytes);
+	// printf("Received header, bytes = %d\n", header.bytes);
+
+	// Send confiration signal
+	if (send_signal(socket_fd, GOT_HEADER)) {
+		goto exit;
+	}
 
 	// Allocate space for data
 	*buf = malloc(header.bytes);
@@ -107,7 +123,28 @@ int tf_recv(int socket_fd, void **buf, size_t *size_bytes, int *term) {
 	}
 
 	// Wait for data
-	goto cleanup_buf;
+	int total_bytes_received = 0;
+	char *next = *buf;
+	while(total_bytes_received < header.bytes) {
+		bytes_received = recv(socket_fd, next, header.bytes - total_bytes_received, 0);
+		if (bytes_received == 0) {
+			*term = 1;
+			goto cleanup_buf;
+		}
+
+		total_bytes_received += bytes_received;
+		next = next + bytes_received;
+	}
+
+	if (total_bytes_received != header.bytes) {
+		goto cleanup_buf;
+	}
+	// printf("Received all %d bytes\n", header.bytes);
+
+	// Send confiration signal
+	if (send_signal(socket_fd, GOT_DATA)) {
+		goto cleanup_buf;
+	}
 
 	return 0;
 
