c25c9b1ef61d21a5a3a4280d9b62410641f51787
Ben Parham <benparham391@gmail.com>
2014-06-10 11:22:16 -0400

    Simplifying directory structure

--- serv/include/transfer.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TRANSFER_H_
-#define _TRANSFER_H_
-
-int tf_send(int socket_fd, const void *buf, size_t size_bytes);
-int tf_recv(int socket_fd, void **buf, size_t *size_bytes, int *term);
-
-int tf_send_file(int socket_fd, char *file_name);
-
-#endif
